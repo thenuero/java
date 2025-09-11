548bd2b70fc1a4e9ba7d54507b07a3d44a4a5049
Nikhil Tripathy <nikhilt231@gmail.com>
2025-09-11 06:42:44 +0530

    update SortColors.java

---
+++ src/com/nikhiltripathy/test/SortColors.java
@@ -0,0 +1,17 @@
+package com.nikhiltripathy.test;
+
+/**
+ * Created on 10/09/2025 at 12:27
+ * Author: theneuro
+ */
+
+public class SortColors {
+    public static void main(String[] args) {
+        char[] input = new char[]{'r', 'r', 'g','b' ,'b', 'g', 'r', 'r', 'b', 'g'};
+        int posR = Integer.MIN_VALUE;
+        int posB = Integer.MIN_VALUE;
+
+        for (int i=0;i<input.length;i++) {
+        }
+    }
+}
